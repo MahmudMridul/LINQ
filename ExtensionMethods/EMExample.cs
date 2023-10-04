dfeea969b0046e24aed1bd9e1178d22c1787165e
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-10-04 09:04:12 +0600

    Extension method example modified

--- ExtensionMethods/EMExample.cs
+++ ExtensionMethods/EMExample.cs
@@ -8,6 +8,14 @@ namespace LINQ.ExtensionMethods
 {
     public static class EMExample
     {
+        public static void ShowEMExample()
+        {
+            string str = "the quick brown fox jumps over the lazy dog";
+            Console.WriteLine(str.SpaceCount());
+            int[] nums = { 12, 34, 54, 12, 23, 77, 22, 98, 123, 56, 32, 97 };
+            Console.WriteLine(nums.CountGreater(50));
+        }
+
         public static int SpaceCount(this string input)
         {
             int count = 0;
--- Program.cs
+++ Program.cs
@@ -6,11 +6,7 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-            string str = "the quick brown fox jumps over the lazy dog";
-            Console.WriteLine(str.SpaceCount());
-            int[] nums = { 12, 34, 54, 12, 23, 77, 22, 98, 123, 56, 32, 97 };
-            Console.WriteLine(nums.CountGreater(50));
-
+            EMExample.ShowEMExample();
         }
     }
 }
