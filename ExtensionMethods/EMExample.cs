fc2f53424c380728035a8f9edfc81b4a14298c02
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-10-04 08:46:16 +0600

    Extension method

---
+++ ExtensionMethods/EMExample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.ExtensionMethods
+{
+    public static class EMExample
+    {
+        public static int SpaceCount(this string input)
+        {
+            int count = 0;
+            for(int i = 0; i < input.Length; ++i)
+            {
+                if (input[i] == ' ') 
+                {
+                    ++count;
+                }
+            }
+            return count;
+        }
+
+        public static int CountGreater(this int[] input, int value)
+        {
+            int count = 0;
+            for(int i = 0; i < input.Length; ++i)
+            {
+                if (input[i] > value)
+                {
+                    ++count;
+                }
+            }
+            return count;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace LINQ
+﻿using LINQ.ExtensionMethods;
+
+namespace LINQ
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string str = "the quick brown fox jumps over the lazy dog";
+            Console.WriteLine(str.SpaceCount());
+            int[] nums = { 12, 34, 54, 12, 23, 77, 22, 98, 123, 56, 32, 97 };
+            Console.WriteLine(nums.CountGreater(50));
+
         }
     }
 }
