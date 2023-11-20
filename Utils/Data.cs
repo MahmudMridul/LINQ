11c2e1aac8228c915cc2a23d486691082a1341a3
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-20 18:50:41 +0600

    Add empty int array, null int array, people array

--- Utils/Data.cs
+++ Utils/Data.cs
@@ -2,6 +2,10 @@
 {
     public static class Data
     {
+        public static int[] nullNums = null;
+        public static int[] emptyNums = { };
+        public static int[] nums = { 54, 28, 73, 11, 92, 67, 5, 39, 88, 16, 81, 43, 76, 22, 98 };
+
         public static Person[] students = {
             new Person(101, "Kamal", "Student", 20, "Single"),
             new Person(102, "Jamal", "Student", 21, "Single"),
@@ -9,5 +13,26 @@
             new Person(104, "Mamal", "Student", 22, "Single"),
             new Person(105, "Samal", "Student", 15, "Single"),
         };
+
+        public static Person[] people = {
+            new Person(1, "Alice", "Engineer", 30, "Single"),
+            new Person(2, "Bob", "Doctor", 35, "Married"),
+            new Person(3, "Charlie", "Teacher", 25, "Single"),
+            new Person(4, "David", "Lawyer", 28, "Married"),
+            new Person(5, "Eva", "Artist", 32, "Single"),
+            new Person(6, "Frank", "Engineer", 40, "Single"),
+            new Person(7, "Grace", "Doctor", 45, "Married"),
+            new Person(8, "Hannah", "Chef", 27, "Single"),
+            new Person(9, "Ian", "Teacher", 29, "Married"),
+            new Person(10, "Julia", "Artist", 31, "Single"),
+            new Person(11, "Kevin", "Engineer", 33, "Married"),
+            new Person(12, "Lily", "Artist", 26, "Single"),
+            new Person(13, "Mike", "Lawyer", 38, "Married"),
+            new Person(14, "Nora", "Chef", 29, "Single"),
+            new Person(15, "Olivia", "Doctor", 36, "Single"),
+            new Person(16, "Kamal", "Student", 20, "Single"),
+            new Person(17, "Jamal", "Student", 21, "Single"),
+        };
+
     }
 }
