a66d9df873f73cf9caf7d13f6979786b46462664
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-20 17:52:13 +0600

    Add Person class

---
+++ Utils/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace LINQ.Utils
+{
+    public class Person
+    {
+        public int NID { get; set; }
+        public string Name { get; set; }
+        public string Profession { get; set; }
+        public int Age { get; set; }
+        public string MaritalStatus { get; set; }
+
+        public Person(int nid, string name, string profession, int age, string maritalStatus)
+        {
+            NID = nid;
+            Name = name;
+            Profession = profession;
+            Age = age;
+            MaritalStatus = maritalStatus;
+        }
+    }
+}
