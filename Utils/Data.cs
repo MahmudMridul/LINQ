bc367dd4487f2f998e59479337da41e23854c057
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-20 20:23:27 +0600

    Update data

--- Utils/Data.cs
+++ Utils/Data.cs
@@ -7,11 +7,11 @@
         public static int[] nums = { 54, 28, 73, 11, 92, 67, 5, 39, 88, 16, 81, 43, 76, 22, 98 };
 
         public static Person[] students = {
-            new Person(101, "Kamal", "Student", 20, "Single"),
-            new Person(102, "Jamal", "Student", 21, "Single"),
-            new Person(103, "Dhamal", "Student", 19, "Single"),
-            new Person(104, "Mamal", "Student", 22, "Single"),
-            new Person(105, "Samal", "Student", 15, "Single"),
+            new Person(16, "Lena", "Student", 18, "Single"),
+            new Person(17, "Ethan", "Student", 20, "Single"),
+            new Person(18, "Sophie", "Student", 22, "Single"),
+            new Person(19, "Lucas", "Student", 19, "Single"),
+            new Person(20, "Aria", "Student", 21, "Single"),
         };
 
         public static Person[] people = {
@@ -30,8 +30,8 @@
             new Person(13, "Mike", "Lawyer", 38, "Married"),
             new Person(14, "Nora", "Chef", 29, "Single"),
             new Person(15, "Olivia", "Doctor", 36, "Single"),
-            new Person(16, "Kamal", "Student", 20, "Single"),
-            new Person(17, "Jamal", "Student", 21, "Single"),
+            new Person(16, "Lena", "Student", 18, "Single"),
+            new Person(17, "Ethan", "Student", 20, "Single"),
         };
 
     }
