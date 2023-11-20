38b69ada61978d3be6fadd1216f456de84acd880
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-20 17:51:45 +0600

    Add Data class

--- Examples/Methods/Select.cs
+++ Examples/Methods/Select.cs
@@ -1,5 +1,4 @@
-﻿
-using LINQ.Utils;
+﻿using LINQ.Utils;
 
 namespace LINQ.Examples.Methods
 {
@@ -12,34 +11,10 @@ namespace LINQ.Examples.Methods
             var squaredNums = nums.Select(n => n * n);
             Printer.Print<int>(squaredNums, nameof(squaredNums));
 
-            Person[] students = { 
-                new Person(101, "Kamal", "Student", 20, "Single"),
-                new Person(102, "Jamal", "Student", 21, "Single"),
-                new Person(103, "Dhamal", "Student", 19, "Single"),
-                new Person(104, "Mamal", "Student", 22, "Single"),
-                new Person(105, "Samal", "Student", 15, "Single"),
-            };
+            var students = Data.students;
             //Creating anonymous type, mofifying object, creating DTOs
             var onlyNameAndID = students.Select(person => new { ID = person.NID, Name = person.Name });
             Printer.Print<Person>(onlyNameAndID, nameof(onlyNameAndID));
-        }
-    }
-
-    internal class Person
-    {
-        public int NID { get; set; }
-        public string Name { get; set; }
-        public string Profession { get; set; }
-        public int Age { get; set; }
-        public string MaritalStatus { get; set; }
-
-        public Person(int nid, string name, string profession, int age, string maritalStatus) 
-        {
-            NID = nid;
-            Name = name;
-            Profession = profession;
-            Age = age;
-            MaritalStatus = maritalStatus;
         }
     }
 }
---
+++ Utils/Data.cs
@@ -0,0 +1,13 @@
+﻿namespace LINQ.Utils
+{
+    public static class Data
+    {
+        public static Person[] students = {
+            new Person(101, "Kamal", "Student", 20, "Single"),
+            new Person(102, "Jamal", "Student", 21, "Single"),
+            new Person(103, "Dhamal", "Student", 19, "Single"),
+            new Person(104, "Mamal", "Student", 22, "Single"),
+            new Person(105, "Samal", "Student", 15, "Single"),
+        };
+    }
+}
