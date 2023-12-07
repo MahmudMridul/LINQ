be3de5bad5251dbb10b67d15cbfe8509271a3754
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-12-07 11:00:19 +0600

    Add Aggregate

---
+++ Examples/Methods/Aggregate.cs
@@ -0,0 +1,29 @@
+﻿
+
+using LINQ.Utils;
+
+namespace LINQ.Examples.Methods
+{
+    public class Aggregate : ILinqFunctions
+    {
+        public void Run()
+        {
+            int[] nums = Data.nums;
+            //Get sum of nums
+            int sum = nums.Aggregate((sum, next) => sum + next);
+            Printer.Print(sum, nameof(sum));
+
+            string[] names = Data.names;
+            //Get longest word from names
+            string longestName = names.Aggregate((longest, next) => longest.Length >= next.Length ? longest : next);
+            Printer.Print(longestName, nameof(longestName));
+
+            //Get an enumerable of length of names
+            IEnumerable<int> lengthOfNames = names.Aggregate(
+                Enumerable.Empty<int>(), 
+                (nameLengths, next) => nameLengths.Append(next.Length)
+            );
+            Printer.Print(lengthOfNames, nameof(lengthOfNames));
+        }
+    }
+}
