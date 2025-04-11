1ddd6fb526f42581c306052868cfbb62fc19c72c
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-04-11 08:46:55 +0600

    add All example #2

--- Examples/Methods/All.cs
+++ Examples/Methods/All.cs
@@ -3,14 +3,18 @@
 
 namespace LINQ.Examples.Methods
 {
-    internal class All : ILinqFunctions
+    public class All
     {
-        public void Run()
+        public static void Run()
         {
             int[] nums = { 1, 2, 3, 6, 5, 3, 8, 10 };
             //checks if all the items in the IEnumerable meets the condition
             bool allArePositive = nums.All(num => num > 0);
             Printer.Print<bool>(allArePositive, nameof(allArePositive));
+
+            string[] names = { "Alice", "Alex", "Baily", "Bob", "Barley" };
+            bool allStartsWithB = names.All(name => name.StartsWith('B'));
+            Printer.Print<bool>(allStartsWithB, nameof(allStartsWithB));
         }
     }
 }
